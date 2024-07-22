a47326fa33446b643a41c33f5cc6f13ddf402506
Kenneth <63093454+Chinorea@users.noreply.github.com>
2024-07-23 00:44:54 +0800

    Fix logic for Datepicker logic
    Fixed logic for Analytics & Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -30,6 +30,8 @@ import DropdownFilter from '../components/DropdownFilter';
 function Dashboard() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [currentuid, setCurrentUser] = useState("");
+  const [DateRange, setDateRange] = useState([]);
+  const [Days, setDays] = useState(7);
 
   const navigate = useNavigate();
 
@@ -123,6 +125,7 @@ function Dashboard() {
                 {/* Filter button */}
                 <DropdownFilter dashboardState={dashboardState} updateDashboardState={updateDashboardState}/>
                 {/* Datepicker built with flatpickr */}
+                <Datepicker Days={Days} setDateRange={setDateRange}/>
                 
                 {/* Add view button */}
                 <AddView currentUid={currentuid} updateDashboardState={updateDashboardState} />               
