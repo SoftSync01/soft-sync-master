2368445182afc16d95e642ad432760b6774cb779
panomia01 <72587556+panomia01@users.noreply.github.com>
2024-07-23 01:15:59 +0800

    added empty tabs into the extra tabs

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,19 @@ import Dashboard from './pages/Dashboard';
 import Analytics from './pages/Analytics';
 import Login from './pages/Login';
 import DatabaseTest from './pages/Database Test';
-import { DndContext } from '@dnd-kit/core';
 import DnDTest from './pages/DnDTest';
+import PoS from './pages/Commerce/PoS';
+import Invoices from './pages/Commerce/Invoices';
+import Shop from './pages/Commerce/Shop';
+import Users from './pages/Community/Users';
+import Profile from './pages/Community/Profile';
+import Employees from './pages/Community/Employees';
+import List from './pages/Task/List';
+import Message from './pages/Message';
+import Inbox from './pages/Inbox';
+import Calendar from './pages/Calendar';
+import Account from './pages/Settings/Account';
+import Noti from './pages/Settings/Noti';
 
 function App() {
 
@@ -39,6 +50,18 @@ function App() {
           <Route exact path="/dashboard/analytics" element={<Analytics />} />
           <Route exact path="/database" element={<DatabaseTest />} />
           <Route exact path="/DnDTest" element={<DnDTest />} />
+          <Route exact path="/commerce/PoS" element={<PoS />} />
+          <Route exact path="/commerce/invoices" element={<Invoices />} />
+          <Route exact path="/commerce/shop" element={<Shop />} />
+          <Route exact path="/community/users" element={<Users />} />
+          <Route exact path="/community/profile" element={<Profile />} />
+          <Route exact path="/community/employees" element={<Employees />} />
+          <Route exact path="/tasks/list" element={<List />} />
+          <Route exact path="/messages" element={<Message />} />
+          <Route exact path="/inbox" element={<Inbox />} />
+          <Route exact path="/calendar" element={<Calendar />} />
+          <Route exact path="/settings/account" element={<Account />} />
+          <Route exact path="/settings/notifications" element={<Noti />} />
 
       </Routes>
     </>
--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -228,7 +228,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                           <li className="mb-1 last:mb-0">
                             <NavLink
                               end
-                              to="/ecommerce/PoS"
+                              to="/commerce/PoS"
                               className={({ isActive }) =>
                                 'block transition duration-150 truncate ' + (isActive ? 'text-indigo-500' : 'text-slate-400 hover:text-slate-200')
                               }
@@ -241,7 +241,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                           <li className="mb-1 last:mb-0">
                             <NavLink
                               end
-                              to="/ecommerce/invoices"
+                              to="/commerce/invoices"
                               className={({ isActive }) =>
                                 'block transition duration-150 truncate ' + (isActive ? 'text-indigo-500' : 'text-slate-400 hover:text-slate-200')
                               }
@@ -254,7 +254,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                           <li className="mb-1 last:mb-0">
                             <NavLink
                               end
-                              to="/ecommerce/shop"
+                              to="/commerce/shop"
                               className={({ isActive }) =>
                                 'block transition duration-150 truncate ' + (isActive ? 'text-indigo-500' : 'text-slate-400 hover:text-slate-200')
                               }
