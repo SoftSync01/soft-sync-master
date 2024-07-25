0a6c31286fcc23ecc9e266c930428a9aae167795
panomia01 <72587556+panomia01@users.noreply.github.com>
2024-07-26 00:36:02 +0800

    test

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -46,6 +46,7 @@ function Dashboard() {
     { id: 'card12', visible: true, component: DashboardCard12, position: 12 },
     { id: 'card13', visible: true, component: DashboardCard13, position: 13 },
   ]);
+  
   const [DateRange, setDateRange] = useState([]);
   const [Days, setDays] = useState(7);
 
